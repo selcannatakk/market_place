657a56682cd4f88d78291b0814c1b3247b96cc06
Selcan Atak <192803025@ogr.cbu.edu.tr>
2024-02-18 14:28:44 +0300

    edited item

--- app/item/urls.py
+++ app/item/urls.py
@@ -6,6 +6,7 @@ app_name = 'item'
 urlpatterns = [
     path("new/", views.new_item, name='new_item'),
     path("<int:pk>", views.detail, name='detail'),
-    path("<int:pk>/delete/", views.delete, name='delete'),
+    path("<int:pk>/delete/", views.delete_item, name='delete_item'),
+    path("<int:pk>/edit/", views.edit_item, name='edit_item'),
 
 ]
--- app/item/views.py
+++ app/item/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect,get_object_or_404
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -35,9 +35,27 @@ def new_item(request):
     }
 
     return render(request, 'item/form.html', context)
+@login_required
+def edit_item(request,pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    context = {
+        'form': form,
+        'title': 'Edit Item'
+    }
+
+    return render(request, 'item/form.html', context)
 
 @login_required
-def delete(request, pk):
+def delete_item(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
 
