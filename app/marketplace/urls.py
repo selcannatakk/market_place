01ce1175b9e841f15ac6981cafe7fb2bb7250475
Selcan Atak <192803025@ogr.cbu.edu.tr>
2024-02-18 03:59:04 +0300

    sign-up page

--- app/marketplace/urls.py
+++ app/marketplace/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'marketplace'
 urlpatterns = [
     path("", views.index, name='index'),
     path("contact/", views.contact, name='contact'),
+    path("signup/", views.signup, name='signup'),
 ]
--- app/marketplace/views.py
+++ app/marketplace/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from item.models import Category, Item
+from .forms import SignupForm
 
 
 def index(request):
@@ -16,3 +17,21 @@ def index(request):
 
 def contact(request):
     return render(request, 'marketplace/contact.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'marketplace/signup.html', context)
