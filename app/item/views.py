bb440c9a24bb6796c2c9517935a28a1ee3d174e1
Selcan Atak <192803025@ogr.cbu.edu.tr>
2024-02-18 15:34:50 +0300

    items search

--- app/item/views.py
+++ app/item/views.py
@@ -1,8 +1,33 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
+
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    context = {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+    }
+
+    return render(request, 'item/items.html', context)
+
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -14,6 +39,7 @@ def detail(request, pk):
     }
 
     return render(request, 'item/detail.html', context)
+
 
 @login_required
 def new_item(request):
@@ -35,8 +61,10 @@ def new_item(request):
     }
 
     return render(request, 'item/form.html', context)
+
+
 @login_required
-def edit_item(request,pk):
+def edit_item(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     if request.method == 'POST':
         form = EditItemForm(request.POST, request.FILES, instance=item)
@@ -53,6 +81,7 @@ def edit_item(request,pk):
     }
 
     return render(request, 'item/form.html', context)
+
 
 @login_required
 def delete_item(request, pk):
