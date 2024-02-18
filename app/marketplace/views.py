c758a136209a0ca9d45625ddf376c938f2f4ad63
Selcan Atak <192803025@ogr.cbu.edu.tr>
2024-02-18 03:22:03 +0300

    Initial commit

---
+++ app/marketplace/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from item.models import Category, Item
+
+
+def index(request):
+    items = Item.objects.filter(is_sold = False)[0:6]
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories,
+        'items': items,
+    }
+    return render(request, 'marketplace/index.html', context)
+
+
+def contact(request):
+    return render(request, 'marketplace/contact.html')
